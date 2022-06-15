549ebd7542dd71b5bffb17e5a5f59d929cc78b92
Lukius <lukiusnatanael@gmail.com>
2022-06-15 23:50:16 +0700

    Add new extension for UIColor

--- BareksaCompare/BareksaCompare/Extensions/UIColor+Extensions.swift
+++ BareksaCompare/BareksaCompare/Extensions/UIColor+Extensions.swift
@@ -5,4 +5,16 @@
 //  Created by BCA-GSIT-MACBOOK-5 on 15/06/22.
 //
 
-import Foundation
+import UIKit
+
+extension UIColor {
+    
+    class var primaryBtnBackground: UIColor {
+        return UIColor(red: 130/255, green: 195/255, blue: 65/255, alpha: 1.0)
+    }
+    
+    class var primaryBtnText: UIColor {
+        return UIColor.black
+    }
+    
+}
