14fcf536095d322831accd7f59a36254b4a1d6bc
Lukius <lukiusnatanael@gmail.com>
2022-07-10 15:59:48 +0700

    Add new color

--- BareksaCompare/BareksaCompare/Extensions/UIColor+Extensions.swift
+++ BareksaCompare/BareksaCompare/Extensions/UIColor+Extensions.swift
@@ -25,6 +25,14 @@ extension UIColor {
         return UIColor(red: 75/255, green: 139/255, blue: 38/255, alpha: 1.0)
     }
     
+    class var greenLine: UIColor {
+        return UIColor(red: 102/255, green: 128/255, blue: 84/255, alpha: 1.0)
+    }
+    
+    class var black10: UIColor {
+        return UIColor(red: 0/255, green: 0/255, blue: 0/255, alpha: 0.1)
+    }
+    
     class var black38: UIColor {
         return UIColor(red: 0/255, green: 0/255, blue: 0/255, alpha: 0.38)
     }
@@ -41,8 +49,16 @@ extension UIColor {
         return UIColor(red: 224/255, green: 219/255, blue: 235/255, alpha: 1.0)
     }
     
+    class var purpleLine: UIColor {
+        return UIColor(red: 114/255, green: 94/255, blue: 156/255, alpha: 1.0)
+    }
+    
     class var navy50: UIColor {
         return UIColor(red: 224/255, green: 232/255, blue: 238/255, alpha: 1.0)
+    }
+    
+    class var navyLine: UIColor {
+        return UIColor(red: 109/255, green: 152/255, blue: 182/255, alpha: 1.0)
     }
     
 }
